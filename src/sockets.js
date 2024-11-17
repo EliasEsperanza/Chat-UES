fa7ec4d8401a7444f5343595296e6e7c3d7f64c0
DieMT10 <diegomorataya03@gmail.com>
2024-11-17 03:48:33 -0600

    Update
    Corrección archivo sockets.js

--- src/sockets.js
+++ src/sockets.js
@@ -1,32 +1,25 @@
 import { registerEvents } from './services/EventServiceProvider.js';
-import { connectDB } from './database/db.js';  // Asegúrate de que la conexión a la base de datos esté bien importada
 
 export default (io) => {
-    // Establecer la conexión a la base de datos
-    connectDB().then(() => {
-        console.log("Connected to MongoDB");
+    // Registrar los eventos
+    registerEvents(io);
 
-        // Registrar los eventos
-        registerEvents(io);
+    io.on("connection", (socket) => {
+        console.log("A user connected");
 
-        io.on("connection", (socket) => {
-            console.log("A user connected");
+        socket.on("client:sendMessage", (data) => {
+            console.log("Message received from client:", data);
 
-            socket.on("client:sendMessage", (data) => {
-                console.log("Message received from client:", data);
-
-                // Emitir el evento 'chat:messageSent' para guardar el mensaje en la base de datos
-                io.emit("chat:messageSent", data);
-            });
-
-            // Manejar la desconexión
-            socket.on("disconnect", () => {
-                console.log("User disconnected");
-            });
+            // Emitir el evento 'chat:messageSent' para guardar el mensaje en la base de datos
+            io.emit("chat:messageSent", data);
         });
-    }).catch((err) => {
-        console.error("Error while connecting to DB:", err);
+
+        // Manejar la desconexión
+        socket.on("disconnect", () => {
+            console.log("User disconnected");
+        });
     });
 };
+
 
 
